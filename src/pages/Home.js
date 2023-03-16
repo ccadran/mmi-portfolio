5dbe2011cac0d9c7743da7529a3d210b3579295d
ccadran <clariocadran33@gmail.com>
2023-03-16 07:12:13 +0100

    add mouse effect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Mouse from "../components/Mouse";
+import Navigation from "../components/Navigation";
 
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <Mouse />
+            <Navigation />
         </div>
     );
 };
