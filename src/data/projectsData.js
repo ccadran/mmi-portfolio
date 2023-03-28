d8ace081fbe23583ab94c8483f84c9dc3c4314e8
ccadran <clariocadran33@gmail.com>
2023-03-28 13:03:52 +0200

    add my projeccts

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,42 +1,38 @@
 export const projectsData = [
-  {
-    id: 1,
-    title: "Casta",
-    date: "Janvier 2020",
-    languages: ["React", "Php", "Sass"],
-    infos:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
-    img: "./assets/img/projet-2.jpg",
-    link: "http://www.google.com",
-  },
-  {
-    id: 2,
-    title: "Lyon Béton",
-    date: "Mars 2020",
-    languages: ["Symfony", "Vue"],
-    infos:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
-    img: "./assets/img/projet-1.jpg",
-    link: "http://www.google.com",
-  },
-  {
-    id: 3,
-    title: "Everpost",
-    date: "Avril 2020",
-    languages: ["Wordpress", "Php", "React"],
-    infos:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
-    img: "./assets/img/projet-3.jpg",
-    link: "http://www.google.com",
-  },
-  {
-    id: 4,
-    title: "Creative Dev",
-    date: "Juillet 2020",
-    languages: ["Vue", "Php"],
-    infos:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
-    img: "./assets/img/projet-4.jpg",
-    link: "http://www.google.com",
-  },
+    {
+        id: 1,
+        title: "Photographie",
+        date: "Septembre 2022",
+        languages: ["Canon EOS 450D", "50mm"],
+        infos: "Projet de Photographie avec pour thème, le mouvement",
+        img: "./assets/img/projet-2.jpg",
+        link: "https://www.linkedin.com/posts/clario-cadran-82b20b209_photos-activity-6983418088897548288-i4kZ?utm_source=share&utm_medium=member_desktop",
+    },
+    {
+        id: 2,
+        title: "Musée de l'automobile",
+        date: "Novembre 2022",
+        languages: ["HTML", "CSS"],
+        infos: "Refonte du site web du musée de l'automobile de Valencay. Une première semaine dédié a l'UI/UX et une deuxième au développement.",
+        img: "./assets/img/projet-1.jpg",
+        link: "https://musee-automobile-valencay.netlify.app/",
+    },
+    {
+        id: 3,
+        title: "Midjourney on rails",
+        date: "Mars 2023",
+        languages: ["Ruby on Rails", "MidJourney"],
+        infos: "Créer un site web avec Ruby on Rails en utilisant des images générées par l'IA de MidJourney. Notre thème a donc été d'imaginer un monde ou les voitures thermiques n'étaient plus utilisées et se retrouvaient a l'abandon.",
+        img: "./assets/img/projet-3.jpg",
+        link: "https://car-world.osc-fr1.scalingo.io/",
+    },
+    {
+        id: 4,
+        title: "Motion Design",
+        date: "Décembre 2022",
+        languages: ["After Effects", "Premier Pro"],
+        infos: "Illustration de la théorie de Emile Durkheim en Motion Design",
+        img: "./assets/img/projet-4.jpg",
+        link: "https://www.youtube.com/watch?v=8pITVMSNv_c&ab_channel=YvanANDRIAMIHAJARIVO",
+    },
 ];
